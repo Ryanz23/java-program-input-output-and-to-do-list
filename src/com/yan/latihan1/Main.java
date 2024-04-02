0d1d63881ae4fdd40e5337968f1c0fa6d18d77d3
Ariyan Andryan Aryja <ariyanandryan@gmail.com>
2024-04-02 23:27:20 +0700

    move directory

--- src/com/yan/latihan1/Main.java
+++ src/com/yan/latihan1/Main.java
@@ -2,7 +2,7 @@ package com.yan.latihan1;
 
 import com.yan.latihan1.latihan.inputoutput.InputOutput;
 import com.yan.latihan1.latihan.Boolean.Boolean;
-import com.yan.latihan1.operator.Operator;
+import com.yan.latihan1.latihan.operator.Operator;
 
 public class Main {
     public static void main(String[] args){
--- src/com/yan/latihan1/latihan/operator/Operator.java
+++ src/com/yan/latihan1/latihan/operator/Operator.java
@@ -1,4 +1,4 @@
-package com.yan.latihan1.operator;
+package com.yan.latihan1.latihan.operator;
 
 import java.util.Scanner;
 
